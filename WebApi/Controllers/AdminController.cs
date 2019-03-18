65b0597062bd677a9705246a856f7d0c34cd990d
SefaCaksu <sefacaksu@gmail.com>
2019-03-18 17:16:45 +0300

    Created admin controller

--- WebApi/Controllers/AdminController.cs
+++ WebApi/Controllers/AdminController.cs
@@ -8,28 +8,159 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    [Route("[Controller]/[Action]")]
     [ApiController]
     public class AdminController : ControllerBase
     {
         readonly ICategory _Category;
+        readonly IProfile _Profile;
+        readonly IArticle _Article;
+        readonly ITag _Tag;
 
-        public AdminController(ICategory Category)
+        public AdminController(ICategory Category, IProfile Profile, IArticle Article, ITag Tag)
         {
             _Category = Category;
+            _Profile = Profile;
+            _Article = Article;
+            _Tag = Tag;
         }
 
+        #region Category
+
+        [Route("Admin/Category")]
         [HttpPost]
-        public string Category([FromBody]string name)
+        public object CategoryAdd([FromBody]string name)
         {
             _Category.Add(name);
             return name;
         }
 
-        [HttpGet]
-        public object Category()
+        [Route("Admin/Category")]
+        [HttpPut]
+        public object CategoryEdit([FromBody] DtoCategory category)
         {
-           return _Category.List(null, true);
+            _Category.Edit(category);
+            return category.Id;
         }
+
+        [Route("Admin/Category")]
+        [HttpGet]
+        public object CategoryList(string name, bool active)
+        {
+            return _Category.List(name, active);
+        }
+
+        [Route("Admin/Category")]
+        [HttpDelete]
+        public object CategoryDelete(int id)
+        {
+            _Category.Delete(id);
+            return id;
+        }
+
+        #endregion
+
+        #region  Profile
+
+        [Route("Admin/Profile")]
+        [HttpGet]
+        public object ProfileGet()
+        {
+            return _Profile.GetById();
+        }
+
+        [Route("Admin/Profile")]
+        [HttpPost]
+        public object ProfileUpsert([FromBody]DtoProfile profile)
+        {
+            _Profile.Upsert(profile);
+            return profile.Id;
+        }
+
+        #endregion
+
+        #region  Article
+
+        [Route("Admin/Article")]
+        [HttpGet]
+        public object ArticleList(string title, int? categoryId, int? tagId, int page, int rowCount)
+        {
+            return _Article.List(title, categoryId, tagId, page, rowCount);
+        }
+
+        [Route("Admin/Article/{id:int}")]
+        [HttpGet]
+        public object ArticleGet(int id)
+        {
+            return _Article.GetById(id);
+        }
+
+        [Route("Admin/Article")]
+        [HttpPost]
+        public object ArticleAdd([FromBody]DtoArticleParams article)
+        {
+            _Article.Add(article);
+            return article.Title;
+        }
+
+        [Route("Admin/Article")]
+        [HttpPut]
+        public object ArticleEdit([FromBody]DtoArticleParams article)
+        {
+            _Article.Update(article);
+            return article.Title;
+        }
+
+        [Route("Admin/Article")]
+        [HttpDelete]
+        public object ArticleDelete(int id)
+        {
+            _Article.Delete(id);
+            return id;
+        }
+
+        [Route("Admin/ArticleCount")]
+        [HttpGet]
+        public object ArticleCount(string title, int? categoryId, int? tagId)
+        {
+            return _Article.Count(title, categoryId, tagId);
+        }
+
+        #endregion
+
+        #region  Tag
+
+        [Route("Admin/Tag")]
+        [HttpGet]
+        public object TagList(string name, bool active)
+        {
+            return _Tag.List(name, active);
+        }
+
+        [Route("Admin/Tag")]
+        [HttpPost]
+        public object TagAdd([FromBody]string name)
+        {
+            _Tag.Add(name);
+            return name;
+        }
+
+
+        [Route("Admin/Tag")]
+        [HttpPut]
+        public object TagEdit([FromBody] DtoTag tag)
+        {
+            _Tag.Edit(tag);
+            return tag.Id;
+        }
+
+        [Route("Admin/Tag")]
+        [HttpDelete]
+        public object TagDelete(int id)
+        {
+            _Tag.Delete(id);
+            return id;
+        }
+
+        #endregion
     }
 }
